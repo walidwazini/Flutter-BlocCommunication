8037b4fa0d9433e50d9a7f099448a29dd6703216
Walid Wazini <wazini25@gmail.com>
2021-12-24 10:54:26 +0800

    stream subscription implement

---
+++ lib/Cubit/internet_cubit.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:bloc_communication/Constants/enums.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:meta/meta.dart';
+
+part 'internet_state.dart';
+
+class InternetCubit extends Cubit<InternetState> {
+  final Connectivity connectivity;
+  StreamSubscription? connectivityStreamSbx;
+
+  InternetCubit({
+    required this.connectivity,
+    // required this.connectivityStreamSbx,
+  }) : super(InternetLoading()){
+    connectivityStreamSbx = connectivity.onConnectivityChanged.listen((connectivityResult) {
+      if (connectivityResult == ConnectivityResult.wifi){
+        emitInternetConnected(ConnectionType.Wifi);
+      } else if (connectivityResult == ConnectivityResult.mobile){
+        emitInternetConnected(ConnectionType.Mobile);
+      } else if (connectivityResult == ConnectivityResult.none){
+        emitInternetDisconnected();
+      }
+
+    });
+  }
+
+  void emitInternetConnected(ConnectionType _selection) => emit(
+        InternetConnected(connectionType: _selection),
+      );
+
+  void emitInternetDisconnected() => emit(InternetDisconnected());
+
+  @override
+  Future<void> close() {
+    // TODO: implement close
+    connectivityStreamSbx!.cancel();
+    return super.close();
+  }
+}
